f704fae3bc43113005dc54807de239eb436eca3c
David Lechner <david.lechner@talsen.team>
2019-01-14 16:04:47 +0100

    add open62541 header and c file

--- bash-open62541/generate-di-types.sh
+++ bash-open62541/generate-di-types.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-mkdir -p ./src_generated/
-
 python3 ./open62541/tools/generate_datatypes.py               \
 --namespace=2                                                 \
 --type-csv=./open62541/deps/ua-nodeset/DI/OpcUaDiModel.csv    \
 --type-bsd=./open62541/deps/ua-nodeset/DI/Opc.Ua.Di.Types.bsd \
 --no-builtin                                                  \
-./src_generated/ua_types_di
+./src/ua_types_di
