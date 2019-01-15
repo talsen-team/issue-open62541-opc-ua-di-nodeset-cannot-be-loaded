85bf35a667681832d0aa8025dc54599c6da62d96
David Lechner <david.lechner@talsen.team>
2019-01-15 20:38:27 +0100

    adjust build routine for open62541 and server application with loaded di information model

--- bash-open62541/build-and-publish-server-app.sh
+++ bash-open62541/build-and-publish-server-app.sh
@@ -2,12 +2,34 @@
 
 set -euo pipefail
 
-cd src
-meson build
+echo ""
+echo "#####"
+echo "building open62541 wih full NS0 ..."
+cd open62541
+mkdir -p build
 cd build
-ninja
-cd ..
+cmake -DUA_ENABLE_FULL_NS0=ON ..
+make
+cp open62541.h ../../src
+cp bin/libopen62541.a ../../src
+cd ../../src
+echo "building open62541 wih full NS0 ... done"
+echo "#####"
+echo ""
+echo ""
+echo "#####"
+echo "building server application with generated nodeset ..."
+gcc -Wall *.c -o server-app -L . -lopen62541
+echo "building server application with generated nodeset ... done"
+echo "#####"
+echo ""
+echo ""
+echo "#####"
+echo "publishing server application with generated nodeset ..."
 cd ..
 mkdir -p publish
 cd publish
-cp ../src/build/server-app .
+cp ../src/server-app .
+echo "publishing server application with generated nodeset ... done"
+echo "#####"
+echo ""
